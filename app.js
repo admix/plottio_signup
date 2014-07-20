7b7049c605548b5f323423be9a2e3666f858661b
admix <admix.snurnikov@gmail.com>
2014-07-20 19:32:25 -0400

    Cleaned up the code

--- app.js
+++ app.js
@@ -52,12 +52,10 @@ MongoClient.connect('mongodb://localhost:27017/blog', function(err, db) {
         dba.testEmail(db, email, function(err, msg) {
           if(err) throw err;
           if(msg == null) {
-            console.log("pre send");
             sendEmail.sendEmail(email);
             res.render(__dirname + "/views/" + 'index',{email:email + " added"});
           } else {
             res.render(__dirname + "/views/" + 'index',{email:email + " already used"});
-            console.log("Already exist");
           }
         })
       } else {
--- public/js/email.js
+++ public/js/email.js
@@ -1,17 +1,12 @@
-console.log("in email");
 var nodemailer = require('nodemailer'),
     habitat = require("habitat");
 
 habitat.load();
-console.log("pre env");
 var env = new habitat(),
     useremail = env.get("EMAIL"),
     userpwd = env.get("PASS");
-console.log("In email.js line 9");
-console.log(useremail +"  "+ userpwd);
 
 function sendEmail(user) {
-  console.log("in send email");
   // create reusable transport method (opens pool of SMTP connections)
   var transporter = nodemailer.createTransport({
       service: "Gmail",
@@ -23,7 +18,6 @@ function sendEmail(user) {
 
 
   var body = "<b>Hi there!</b><ul>Wouldn’t it be great if:<li>You could read/write articles that are created by fellow users in real-time without media bias in between?</li><li>You could show your savvy in areas that you are hooked on and inspire the rest of the world?</li><li>You could read it as an online newspaper interconnected with anyone on the resource?</li><li>You would not have to stick to 140 characters limit?</li></ul><p>You could be a part in our Social Journalism Revolution?✔ </p>"
-  console.log(body);
   // setup e-mail data with unicode symbols
   var mailOptions = {
       from: "Alexander Snurnikov ✔ <admix.software@gmail.com>", // sender address
