3fd92b1e69be6ad70d92ccaebeec727c60adf389
admix <admix.snurnikov@gmail.com>
2014-07-21 21:19:11 -0400

    Added social + fixed design

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ MongoClient.connect('mongodb://localhost:27017/blog', function(err, db) {
           if(msg == null) {
             sendEmail.sendEmail(email);
             res.render(__dirname + "/views/" + 'index',{email:email + " added"});
+            $(".greeting").text("Welcome!");
           } else {
             res.render(__dirname + "/views/" + 'index',{email:email + " already used"});
           }
